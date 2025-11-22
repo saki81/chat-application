6547e06fbeb0953fb2a4818cd5aefcc81e2831f8
unknown <cike198110@hotmail.com>
2025-11-22 14:29:34 +0100

    install and import socket.io into to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,11 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connDB } from "./lib/db";
 import cors from "cors";
+import {  server, app } from "./lib/socket";
 
 dotenv.config();
 
-const app = express();
+
 
 app.use(express.json({limit: '5mb'}));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
@@ -23,7 +24,7 @@ app.use("/api/message", messagesRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
    console.log("Server is running " + PORT);
    connDB();
 });
